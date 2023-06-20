86b4791e106360d9f52266c0287768b40bb53e40
Gabriel <gabriel.araujo2902@hotmail.com>
2023-06-19 23:44:17 -0300

    Color changes

--- contato.php
+++ contato.php
@@ -7,7 +7,10 @@
     <main>
 		<div id="whats-form">
 			<img src="img/contact.webp">
-			<h2>Envie uma mensagem. podemos te ajudar!</h2>
+			<section>
+				<h2>Que tal enviar uma mensagem?</h2>
+				<p>Olá, meu nome é Suellen, sou advogada e quero te ajudar!</p>
+			</section>
 			<br>
 			<form action="whatsapp.php" method="POST">
 				<label for="nome">Qual o seu nome?</label> <input type="text"
